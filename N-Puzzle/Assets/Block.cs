4aa74ee840686774c06e6ed047941545a449ed4d
rmbozhko <yamakasi.romka@yahoo.fr>
2018-07-24 12:25:00 +0300

    Wowking on visualization using Unity
    Creating image sliding game using Unity.
    The picture will be solved using A* star algorithm, which will be
    implemented later.

---
+++ N-Puzzle/Assets/Block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	// each created Quad has Collider property that allows to attach C# script to it
+	public event System.Action<Block>	OnBlockPressed;
+
+	// gets called automatically if collider is attached
+	void	OnMouseDown()
+	{
+		if (OnBlockPressed != null)
+			OnBlockPressed(this);
+	}
+
+}
---
+++ N-Puzzle/Assets/Puzzle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+	public const int 	blocksPerLine = 4;
+	Block				emptyBlock;
+
+	void		Start()
+	{
+		CreatePuzzle ();
+	}
+
+	void		CreatePuzzle()
+	{
+		for (int y = 0; y < blocksPerLine; y++) {
+			for (int x = 0; x < blocksPerLine; x++) {
+				// creating single block in the grid
+				GameObject blockObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+				// setting position of block on the grid
+				blockObject.transform.position = -Vector2.one * (blocksPerLine - 1) * .5f  + new Vector2(x, y);
+				blockObject.transform.parent = transform;
+
+				// adding block script to each of created blockObject-s
+				Block block = blockObject.AddComponent<Block>();
+				block.OnBlockPressed += PlayerMoveBlockInput;
+
+				// hiding block in top right corner
+				if (y == 0 && x == blocksPerLine - 1) {
+					blockObject.SetActive (false);
+					emptyBlock = block;
+				}
+			}
+		}
+
+		// changing the size of the camera, to fit grid nearly completely
+		Camera.main.orthographicSize = blocksPerLine * 0.55f;
+	}
+
+	void		PlayerMoveBlockInput(Block blockToMove)
+	{
+		// swapping blocks when pressed
+		Vector2		targetPosition = emptyBlock.transform.position;
+		emptyBlock.transform.position = blockToMove.transform.position;
+		blockToMove.transform.position = targetPosition;
+	}
+}
