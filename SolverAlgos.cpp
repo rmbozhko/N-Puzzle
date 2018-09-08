69704ebeb2689bfb4acf7f0ca0489efd477eed07
Roman Bozhko <rm.bozhko@gmail.com>
2018-09-08 15:10:18 +0300

    * Fixed IDA* for working both for unicost and greedy searches
    * In case of unicost search the described bug of greedy mode was in place.

--- SolverAlgos.cpp
+++ SolverAlgos.cpp
@@ -68,36 +68,29 @@ NPuzzle::State* 		NPuzzle::Solver::StatesDeepening(NPuzzle::State* temp, const s
 {
 	std::vector<NPuzzle::State*>		children;
 	float								tempFCost = std::numeric_limits<float>::max();
-	size_t								pos = 0;
+	State*								stmt;
+	State*								st;
 
-	if (threshold < temp->GetFCost() || *(temp) == Solver::GetFinalState())
-	{
-		// if (*(temp) == Solver::GetFinalState())
-			// std::cout << "@@@@@@@@@@@@Found\n" << temp << std::endl;	
+	if (threshold < temp->GetFCost() || *(temp) == Solver::GetFinalState()) {
 		return temp;
 	}
-	// std::cout << "?????????? Next node\n" << temp << std::endl;
+
 	children = temp->GetChildren(GetPuzzleSize(), *this);
-	for (size_t i = 0; i < children.size(); ++i)
-	{
-		// std::cout << "---------------" << std::endl;
-		// std::cout << children[i] << std::endl;
-		// std::cout << "###############" << std::endl;
-		State*		st = StatesDeepening(children[i], threshold);
-		if (*(st) == Solver::GetFinalState())
-		{
-			// std::cout << "$$$$$$$$$$$$$$$$$$$$$Found\n" << st << std::endl;
-			return (st);//(children[i]);
+	
+	for (size_t i = 0; i < children.size(); ++i) {
+		
+		st = StatesDeepening(children[i], threshold);
+		
+		if (*(st) == Solver::GetFinalState()) {
+			return (st);
 		}
-		if (children[i]->GetFCost() < tempFCost)
-		{
-			// std::cout << "Cost is less for child #" << i << std::endl << children[i];
-			tempFCost = children[i]->GetFCost();
-			pos = i;
+		
+		if (st->GetFCost() < tempFCost) {
+			tempFCost = st->GetFCost();
+			stmt = st;
 		}
 	}
-	// std::cout << "Choosed child #" << pos << std::endl << children[pos];
-	return (children[pos]);
+	return (stmt);
 }
 
 NPuzzle::State* 		NPuzzle::Solver::SolveWithIDA(NPuzzle::State* start)
@@ -106,19 +99,13 @@ NPuzzle::State* 		NPuzzle::Solver::SolveWithIDA(NPuzzle::State* start)
 	float					threshold;
 
 	threshold = start->GetFCost();
-	SetUnicostMode(false);
 	if (*(start) == Solver::GetFinalState())
 			return (start);
 	while (1)
 	{
-		// std::cout << threshold << std::endl;
-		// std::cout << "Start:\n" << start << std::endl;
 		temp = NPuzzle::Solver::StatesDeepening(start, threshold);
 		if (*(temp) == Solver::GetFinalState())
 			return (temp);
-		// std::cout << "Moving along" << std::endl;
 		threshold = temp->GetFCost();
-		// if (threshold )
-			// exit(0);
 	}
 }
