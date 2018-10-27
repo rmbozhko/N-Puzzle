74a06abe56869983f6442b4c4243868aba307b8a
Roman Bozhko <rm.bozhko@gmail.com>
2018-10-27 18:36:19 +0300

    Merging Unity part with C++

--- get_img.sh
+++ get_img.sh
@@ -8,7 +8,7 @@ do
 
 	# photos can also be small_, medium_, large_ 
 	MyFile=$(basename $username)	                   		# strip the filename
-	MyLink='https://cdn.intra.42.fr/users/'$MyFile'.jpg' 	# form proper link for downloading
-	#/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-	./N-Puzzle/n_puzzle "-img=" MyLink "-size=" p_size
+	MyLink='https://cdn.intra.42.fr/users/medium_'$MyFile'.jpg' 	# form proper link for downloading
+
+	open -a /Users/rbozhko/Desktop/projects/n_puz/N-Puzzle/unity_n_puzzle.app --args "-img" $MyLink "-size" $p_size
 done
